5c047505996136e102273de5bd916591c36c2df0
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2024-03-19 08:51:00 +0530

    .

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 
 class ProjectForm(ModelForm):
@@ -13,3 +14,13 @@ class ProjectForm(ModelForm):
             "source_link",
             "tags",
         ]
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
+        # self.fields["title"].widget.attrs.update({"class": "input"})
