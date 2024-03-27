7909f91571390515cf9aa3cecaa37053505d02c4
BhatFaheem <99001563+BhatFaheem@users.noreply.github.com>
2024-03-27 13:06:52 +0530

    .

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 
 def loginUser(request):
+    page = "login"
     if request.user.is_authenticated:
         return redirect("profiles")
 
@@ -29,8 +31,26 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, "User was successfully logged out")
+    messages.info(request, "User was successfully logged out")
     return redirect("/login")
+
+
+def registerUser(request):
+    page = "register"
+    form = CustomUserCreationForm
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, "User account was created")
+            login(request, user)
+            return redirect("profiles")
+        else:
+            messages.error(request, "Error Occured")
+    context = {"page": page, "form": form}
+    return render(request, "users/login_register.html", context)
 
 
 def profiles(request):
